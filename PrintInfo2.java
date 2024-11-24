c71dcf9f6700e7ffd3c7b960778627524f5956e7
hh-123-hh <hooj1027@naver.com>
2024-11-24 17:36:18 +0900

    Add files via upload

---
+++ PrintInfo2.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class PrintInfo2 {// 클래스 중괄호의 시작 영역
+		public static void main (String[] args){ // 메인 메소드 시작 영역
+			System.out.print(" 2024년 11월 24일 "); // 현재 과제를 작성하고 있는 날짜 입력. 줄바꿈이 되지 않도록 print 메소드 사용
+			System.out.println(" 오전 11:56분 ");	// 현재 과제를 작성하고 있는 시간 입력. 뒤에 따로 내용이 없어서 줄바꿈이 되어도 상관 없어서 println 메소드 사용하여 시간 값 출력 진행함
+		} // 메인 메소드 중괄호 마침 영역
+} // 클래스 중괄호의 마침 영역
