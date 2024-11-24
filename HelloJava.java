4719f4627583af0ef4456de1d625af35ab498f5a
hh-123-hh <hooj1027@naver.com>
2024-11-24 17:02:16 +0900

    241124 17시 02분 복습 후 과제 제출

---
+++ HelloJava.java
@@ -0,0 +1,7 @@
+package homework; // 패키지 명
+
+public class HelloJava { // 클래스 중괄호 첫 시작 영역
+	public static void main(String[] args) { // 메인메소드 중괄호 시작 영역
+		System.out.print("HelloJava"); // HelloJava 라는 입력값을 출력하기 위해 출력 메소드 사용
+	} // 메인 메소드 중괄호 마침 영역
+} // 클래스 중괄호 마침 영역
---
+++ PrintInfo.java
@@ -0,0 +1,7 @@
+package homework;
+
+public class PrintInfo { // 클래스 중괄호 시작 영역
+		public static void main(String[] args) { // 메인 메소드 중괄호 시작 영역
+			System.out.println("강후현, 32살"); // 출력메소드를 사용하여 이름, 나이 값을 입력
+		} // 메인 메소드 중괄호 마침 영역
+} // 클래스 중괄호 마침 영역
